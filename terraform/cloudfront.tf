8991765654e3b1859fdd59bafafa21e37c1c4a82
geekcu <geekcu97@gmail.com>
2023-12-13 19:49:48 -0500

    somechange

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -89,7 +89,7 @@ resource "aws_s3control_multi_region_access_point" "multibucket" {
 }
 
 resource "aws_cloudfront_origin_access_control" "OAC" {
-  name                              = "TechScrum OAC"
+  name                              = "TechScrum-UAT OAC"
   description                       = "description of OAC"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
